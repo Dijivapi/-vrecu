632eccab43f2375f68739d3c74f4eeb7e06c009f
Diana <dianitavan@gmail.com>
2020-08-18 18:56:52 -0500

    correción al puerto

--- app.js
+++ app.js
@@ -34,4 +34,6 @@ app.get("/", async(req, res) => {
     res.render("index", { visitors: visitors })
 });
 
-app.listen(3000, () => console.log("Listening on port 3000 ..."));
+app.listen(3000, () => {
+    return console.log('Listening on port 3000!');
+});
